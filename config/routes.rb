99b8692c5e8c01d4caecb6cfea8d63c1a653bb42
Ritesh <railsriteshc56@gmail.com>
2019-11-28 12:13:07 +0530

    added user and questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :questions
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
