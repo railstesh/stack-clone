4b9f6910a701c0c08e95e93d0e14e917485a7d29
Ritesh <railsriteshc56@gmail.com>
2019-11-29 11:59:33 +0530

    added routes and schema file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :questions
+  resources :questions do
+    resources :answers
+  end
   devise_for :users
+  resources :answers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'all_questions', to: 'questions#all_questions'
+  root 'questions#all_questions'
 end
