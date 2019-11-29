75537f6ae05183346001ce850d9f20d3a95ad10f
Ritesh <railsriteshc56@gmail.com>
2019-11-29 14:42:02 +0530

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,55 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database
-#   with its default values.
-# The data can then be loaded with the rails db:seed command (or created
-#   alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' },
-#     { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.first_or_create!(
+  email: "test1@yopmail.com",
+  password: '123456',
+  password_confirmation: '123456'
+)
+User.first_or_create!(
+  email: "test2@yopmail.com",
+  password: '123456',
+  password_confirmation: '123456'
+)
+
+Question.create(
+  title: "How to run rspec rails application tests",
+  description: 'How to run rspec rails application tests',
+  user_id: User.first.id
+)
+Question.create(
+  title: "How to run rails application server",
+  description: 'How to run rails application server',
+  user_id: User.first.id
+)
+
+Answer.create(
+  description: 'run rspec spec/',
+  question_id: Question.first.id,
+  user_id: User.first.id
+)
+Answer.create(
+  description: 'run bundle exec rspec spec/',
+  question_id: Question.first.id,
+  user_id: User.first.id
+)
+Answer.create(
+  description: 'run command rspec spec/ and spec name',
+  question_id: Question.first.id,
+  user_id: User.first.id
+)
+
+Answer.create(
+  description: 'run rails server',
+  question_id: Question.last.id,
+  user_id: User.first.id
+)
+Answer.create(
+  description: 'run bundle exec rails s',
+  question_id: Question.last.id,
+  user_id: User.first.id
+)
+Answer.create(
+  description: 'run rails s on terminal',
+  question_id: Question.last.id,
+  user_id: User.first.id
+)
