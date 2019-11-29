a331c7342ecf0da94ab021028b46450961e4d6dd
Ritesh <railsriteshc56@gmail.com>
2019-11-29 15:04:07 +0530

    Enabled brute force attack protection

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :lockable
   has_many :questions
   has_many :answers
 end
